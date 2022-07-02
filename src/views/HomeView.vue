eccf02ba0fe6b10e38fab60d9c2be89813e3ea1d
serghine <sergh.boudj@gmail.com>
2022-07-02 19:48:44 +0200

    mise en place de l'environement de travail

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+  <h1> Récépions </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
